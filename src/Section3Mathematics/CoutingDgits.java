370b1fd81643feda99dd237dbd53af39cb91c930
Vishal Khairnar <vishalkhairnar700@gmail.com>
2025-07-22 22:39:28 +0530

    Count digits progam in java added

---
+++ src/Section3Mathematics/CoutingDgits.java
@@ -0,0 +1,28 @@
+package Section3Mathematics;
+
+import java.util.Scanner;
+
+public class CoutingDgits {
+    public static void countingDigits(int num){
+        if(num < 0) {
+            System.out.println("Please Enter valid positive integer");
+
+        }
+
+        int count = 0;
+        while(num > 0){
+            num = num/10;
+            count++;
+        }
+
+        System.out.println("Total digits in number are: "+count);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a Positive Integer");
+        int number = scan.nextInt();
+        countingDigits(number);
+    }
+
+}
