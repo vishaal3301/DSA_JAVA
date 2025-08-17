d36ce963613c3244e5623efc6fef425456e4f7ac
Vishal Khairnar <vishalkhairnar700@gmail.com>
2025-08-18 00:16:38 +0530

    Bit set count Array 1 added

--- src/Section4BitMagic/BitSetCountArray1.java
+++ src/Section4BitMagic/BitSetCountArray1.java
@@ -1,9 +1,9 @@
 package Section4BitMagic;
 
-public class BitSetCountArray {
+public class BitSetCountArray1 {
     public static int[] tbl = new int[256];
 
-    public BitSetCountArray(){
+    public BitSetCountArray1(){
         tbl[0] = 0;
         for(int i=1; i<256;i++){
             tbl[i] = tbl[i & (i-1)] +1;
@@ -19,7 +19,7 @@ public class BitSetCountArray {
 
     public static void main(String[] args) {
         int num = 13;
-        new BitSetCountArray();
+        new BitSetCountArray1();
         System.out.println("Number :"+num);
         System.out.println("Binary :"+Integer.toBinaryString(num));
         System.out.println("Set Bits : "+countSetBits(num));
